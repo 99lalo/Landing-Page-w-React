d6f1e96bb300a2a9d9938560b6757c5456bd663b
99lalo <xx66laloxx@gmail.com>
2021-01-07 18:09:53 +0000

    Updates

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
-
+import { Navbar } from "./component/navbar.js";
+import { Content } from "./component/content.js";
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Navbar />, <Content />, document.querySelector("#app"));
