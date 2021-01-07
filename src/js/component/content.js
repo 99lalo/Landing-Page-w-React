882dd8bcb4efc12cdc9228c7ddb270e04b74b040
99lalo <xx66laloxx@gmail.com>
2021-01-07 20:09:01 +0000

    components were missing

---
+++ src/js/component/content.js
@@ -0,0 +1,109 @@
+import React from "react";
+
+export function Content() {
+	return (
+		<div className="container">
+			<div className="jumbotron pt-0">
+				<h1 className="display-4">A Warm Welcome!</h1>
+				<p className="lead">
+					<b>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Proin pretium purus ante, sit amet tempus dolor
+						malesuada vel. Donec tempor elit velit, maximus rhoncus
+						urna imperdiet porta
+					</b>
+				</p>
+				<p className="lead">
+					<a
+						className="btn btn-primary btn-lg"
+						href="#"
+						role="button">
+						Call to action!
+					</a>
+				</p>
+			</div>
+			<div className="row justify-content-between m-1">
+				<div className="card" style={{ width: "16rem" }}>
+					<img
+						className="card-img-top"
+						src="https://cdn2.hubspot.net/hub/1927341/hubfs/image-heat-for-the-holidays-toasty-fire-500x325px.jpg?width=750&name=image-heat-for-the-holidays-toasty-fire-500x325px.jpg"
+						alt="Card image cap"
+					/>
+					<div className="card-body text-center">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Proin pretium purus ante, sit amet tempus
+							dolor malesuada vel.
+						</p>
+					</div>
+					<div className="card-footer d-flex justify-content-center">
+						<a href="#" className="btn btn-primary">
+							Find out more!
+						</a>
+					</div>
+				</div>
+				<div className="card" style={{ width: "16rem" }}>
+					<img
+						className="card-img-top"
+						src="https://cdn2.hubspot.net/hub/1927341/hubfs/image-heat-for-the-holidays-toasty-fire-500x325px.jpg?width=750&name=image-heat-for-the-holidays-toasty-fire-500x325px.jpg"
+						alt="Card image cap"
+					/>
+					<div className="card-body text-center">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Proin pretium purus ante, sit amet tempus
+							dolor malesuada vel.
+						</p>
+					</div>
+					<div className="card-footer d-flex justify-content-center">
+						<a href="#" className="btn btn-primary">
+							Find out more!
+						</a>
+					</div>
+				</div>
+				<div className="card" style={{ width: "16rem" }}>
+					<img
+						className="card-img-top"
+						src="https://cdn2.hubspot.net/hub/1927341/hubfs/image-heat-for-the-holidays-toasty-fire-500x325px.jpg?width=750&name=image-heat-for-the-holidays-toasty-fire-500x325px.jpg"
+						alt="Card image cap"
+					/>
+					<div className="card-body text-center">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Proin pretium purus ante, sit amet tempus
+							dolor malesuada vel.
+						</p>
+					</div>
+					<div className="card-footer d-flex justify-content-center">
+						<a href="#" className="btn btn-primary">
+							Find out more!
+						</a>
+					</div>
+				</div>
+				<div className="card" style={{ width: "16rem" }}>
+					<img
+						className="card-img-top"
+						src="https://cdn2.hubspot.net/hub/1927341/hubfs/image-heat-for-the-holidays-toasty-fire-500x325px.jpg?width=750&name=image-heat-for-the-holidays-toasty-fire-500x325px.jpg"
+						alt="Card image cap"
+					/>
+					<div className="card-body text-center">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Proin pretium purus ante, sit amet tempus
+							dolor malesuada vel.
+						</p>
+					</div>
+					<div className="card-footer d-flex justify-content-center">
+						<a href="#" className="btn btn-primary">
+							Find out more!
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
